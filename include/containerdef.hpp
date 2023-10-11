115379060f33d1b8836047d77a03c19828210f56
Faisal A <akhtarfaisal147@gmail.com>
2023-10-11 10:17:15 +0200

    add String typedef and rename typedefs
    std::string has an alias called 'String'. Also, unique_ptr and
    shared_ptr have been aliased to Box and Rc, respectively.
    
    The names come from Rust's Box and Rc.

--- include/containerdef.hpp
+++ include/containerdef.hpp
@@ -3,16 +3,23 @@
 #include <vector>
 #include <memory>
 #include <stack>
+#include <string>
 
 
 template <typename T>
 using Vec = std::vector<T>;
 
 template <typename T>
-using Unq = std::unique_ptr<T>;
+using Box = std::unique_ptr<T>;
 
 template <typename T>
 using Rc = std::shared_ptr<T>;
 
 template <typename T>
+using Weak = std::weak_ptr<T>;
+
+template <typename T>
 using Stack = std::stack<T>;
+
+
+typedef std::string String;

0a0fbad0f04a911ac85c5d9c2efaa9be1397fe78
Faisal A <akhtarfaisal147@gmail.com>
2023-10-28 17:08:16 +0200

    change layout of folders and project
    Since this is a binary, many of the functionality in the include
    directory will instead be inside the src directory.

--- include/typedef.hpp
+++ include/typedef.hpp
@@ -4,17 +4,17 @@
 #include <cstddef>
 
 
-typedef uint8_t u8;
 typedef  int8_t i8;
+typedef uint8_t u8;
 
-typedef uint16_t u16;
 typedef  int16_t i16;
+typedef uint16_t u16;
 
-typedef uint32_t u32;
 typedef  int32_t i32;
+typedef uint32_t u32;
 
-typedef uint64_t u64;
 typedef  int64_t i64;
+typedef uint64_t u64;
 
 typedef size_t usz;
 
@@ -24,5 +24,14 @@ typedef double f64;
 
 #define local static
 #define loop while(1)
+
+
+#define null ((void*)0)
+
+
+/* Used for conditional statements of the form a < r(x) < b */
+#define r(x) (x) && (x)
+
+
 #define NL "\n"
 #define NLC '\n'
